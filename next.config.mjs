adc8370fd5de4e7da687381ef579279546ee26a1
Tuhin200655 <tuhinsantra200655@gmail.com>
2026-01-18 17:49:17 +0530

    Rebrand to Deal Hunt and implement dashboard

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "rukminim2.flixcart.com",
+      },
+      {
+        protocol: "https",
+        hostname: "fpqtwoehiqdrwrlddtyk.supabase.co",
+      },
+      // Add more domains as needed
+      {
+        protocol: "https",
+        hostname: "m.media-amazon.com"
+      },
+      {
+        protocol: "https",
+        hostname: "images-na.ssl-images-amazon.com"
+      }
+    ],
+  },
 };
 
 export default nextConfig;
